9ee59f3867cadc6e2d172630674a4529c0e09395
rubenspotcake <rubenspotcake@proton.me>
2024-06-06 15:49:17 -0300

    Primeiro commit aulao

---
+++ src/main/java/com/rubens/aulao_spring_security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.rubens.aulao_spring_security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+      return http
+          .authorizeHttpRequests(
+              authorizeConfig -> {
+                authorizeConfig.requestMatchers("/public").permitAll();
+                authorizeConfig.requestMatchers("/logout").permitAll();
+                authorizeConfig.anyRequest().authenticated();
+              })
+            .oauth2Login(Customizer.withDefaults())
+            .oauth2ResourceServer(config -> {
+                config.jwt(Customizer.withDefaults());
+              })
+          .build();
+    }
+    
+}
